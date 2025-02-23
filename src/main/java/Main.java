354a86f023c0fd8e555decca46e5bc63aaeebd72
xabierjj <xabierjj@gmail.com>
2025-02-23 19:39:06 +0100

    return response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 public class Main {
   public static void main(String[] args) {
@@ -16,7 +19,13 @@ public class Main {
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
     
-      serverSocket.accept(); // Wait for connection from client.
+      Socket s = serverSocket.accept(); // Wait for connection from client.
+      OutputStream outputStream =  s.getOutputStream();
+      String response = "HTTP/1.1 200 OK\r\n\r\n";
+      PrintWriter out =
+        new PrintWriter(outputStream, true);
+        out.println(response);
+      out.flush();
       System.out.println("accepted new connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
