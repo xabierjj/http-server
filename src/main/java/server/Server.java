33d2301439ed3ee7895fd40602762a52114b8cb4
xabierjj <xabierjj@gmail.com>
2025-04-17 09:24:48 +0200

    run requests in different threads

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -15,55 +15,73 @@ import java.util.Map;
 import model.HttpRequest;
 import model.HttpResponse;
 import model.HttpStatusCode;
+
 public class Server {
+
     private Router router;
+
     public Server(Router router) {
         this.router = router;
     }
-    
+
     public void start(Integer port) {
-         try {
-      ServerSocket serverSocket = new ServerSocket(port);
 
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
-      serverSocket.setReuseAddress(true);
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
 
-      while (true) {
-        Socket s = serverSocket.accept(); // Wait for connection from client.
-        InputStream in = s.getInputStream();
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-        List<String> requestLines = new ArrayList<>();
-        String line;
-        while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
-          requestLines.add(line);
+            while (true) {
+                Socket s = serverSocket.accept();
+                System.out.println("accepted new connection");
+                this.runThread(s);
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
         }
-        HttpRequest httpRequest = new HttpRequest(requestLines);
-
-        RouteHandler requestHandler = this.router.getHandler(httpRequest.getPath());
-        HttpResponse httpResponse;
-        if (requestHandler==null) {
-          httpResponse = new HttpResponse(httpRequest.getProtocol(), HttpStatusCode.NOT_FOUND);
-        } else {
-          Map<String,String> paramMap = requestHandler.params();
-          httpResponse = requestHandler.requestHandler().handle(httpRequest,paramMap);
-        }
-
-  
-       
-
-        OutputStream outputStream = s.getOutputStream();
-
-        PrintWriter out = new PrintWriter(outputStream, true);
-        out.print(httpResponse.getResponse());
-        out.flush();
-        s.close();
-        System.out.println("accepted new connection");
-      }
-
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
     }
+
+    public void runThread(Socket s) {
+        Thread thread = new Thread(() -> {
+
+            try {
+                InputStream in = s.getInputStream();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+                List<String> requestLines = new ArrayList<>();
+                String line;
+                while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
+                    requestLines.add(line);
+                }
+                HttpRequest httpRequest = new HttpRequest(requestLines);
+
+                RouteHandler requestHandler = this.router.getHandler(httpRequest.getPath());
+                HttpResponse httpResponse;
+                if (requestHandler == null) {
+                    httpResponse = new HttpResponse(httpRequest.getProtocol(), HttpStatusCode.NOT_FOUND);
+                } else {
+                    Map<String, String> paramMap = requestHandler.params();
+                    httpResponse = requestHandler.requestHandler().handle(httpRequest, paramMap);
+                }
+
+                OutputStream outputStream = s.getOutputStream();
+
+                PrintWriter out = new PrintWriter(outputStream, true);
+                out.print(httpResponse.getResponse());
+                out.flush();
+                s.close();
+            } catch (IOException exception) {
+                System.out.println("IOException: " + exception.getMessage());
+
+            }
+
+        });
+
+        thread.start();
     }
+
 }
